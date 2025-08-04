3e643dd95bbee416ab24b3099a6ebc2bafaef814
Ryshan172 <ryshanramlall@outlook.com>
2025-08-04 22:24:16 +0200

    Updated workflow and got initial results for normal methods

---
+++ learn_structure.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from utils.util_functions import *
+from sampler_functions.sampler import *
+from structure_learning.hill_climbing import *
+from structure_learning.simulated_annealing import simulated_annealing_search
+
+def structure_results(method, results):
+    result_summary = ""
+    result_summary += f"Structure Learning Method: {method}\n"
+    result_summary += "Learned Edges:\n"
+
+    # Convert results (likely an OutEdgeView) into a list of edges
+    for edge in list(results):
+        result_summary += f"  {edge[0]} -> {edge[1]}\n"
+
+    return result_summary
+
+
+def learn_structure(dataset_name):
+
+    results_output = ""
+
+    # Generate sample dataset
+    dataset = load_csv_data(dataset_name)
+
+    # Hill Climbing structure learning
+    struc = run_hillclimb_search(dataset)
+
+    # Create and save output
+    results1 = structure_results("Hill Climbing", struc)
+    results_output += results1
+
+
+    # Simulated Annealing Search
+    struc_sa = simulated_annealing_search(dataset)
+    results2 = structure_results("Simulated Annealing", struc_sa)
+    results_output += results2
+
+    # Save final results
+    save_outputs(results_output, dataset_name)
+
+    print("Completed structure learning and saved outputs")
+
+
+def run_workflow():
+    learn_structure('psych_small_lower_samples')
+
+    learn_structure('psych_small_upper_samples')
+
+    learn_structure('psych_small_midpoint_samples')
+
+    learn_structure('psych_small_random_samples')
+
+# Run main method
+run_workflow()
--- utils/util_functions.py
+++ utils/util_functions.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 def load_json_data(filename): 
     # Open and read the JSON file
@@ -9,9 +10,9 @@ def load_json_data(filename):
     return data
 
 
-def save_outputs(results):
+def save_outputs(results, dataset_name):
     # Save text of results
-    with open("outputs/results.txt", "w") as f:
+    with open(f"outputs/{dataset_name}_results.txt", "w") as f:
         f.write(results)
 
 
@@ -19,3 +20,10 @@ def save_sampled_intervals(dataframe, method, lcn_name):
     # Saves sampled dataset as csv 
     filename = f"{lcn_name}_{method}_samples.csv"
     dataframe.to_csv(f"datasets/sampled_data/{filename}", index=False)
+
+
+def load_csv_data(filename):
+    # Loads data from csv and returns dataframe
+    df = pd.read_csv(f"datasets/sampled_data/{filename}.csv")
+
+    return df
