4b2f40516da71b6f33ce760f984851ce93768b48
Ryshan172 <ryshanramlall@outlook.com>
2025-08-09 18:40:10 +0200

    Developing workflow for structure learning random LCNs

--- lcn_functions/model.py
+++ lcn_functions/model.py
@@ -9,11 +9,15 @@ Generating LCNs and testing if they are valid
 def generate_lcn_workflow():
     # Generate small lCN (size 5)
     lcn = generate_lcn(size=5, interval_width=0.3, num_constraints=2, constraint_chaining=True)
-    print(json.dumps(lcn, indent=2))
+    # print(json.dumps(lcn, indent=2))
 
     # Check if valid
     validation = validate_lcn(lcn)
-    print(validation)
+
+    # TODO: Create while loop to keep running until valid
+
+    return lcn
+    
 
 
 # Run function
